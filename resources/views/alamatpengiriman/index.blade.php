51f4aa1f1716ca6c6cadd3d73c86fe939e9dd12d
aikalalfrian <51121861+aikalalfrian@users.noreply.github.com>
2021-08-02 02:32:50 +0700

    update alamat baru

--- resources/views/alamatpengiriman/index.blade.php
+++ resources/views/alamatpengiriman/index.blade.php
@@ -21,7 +21,7 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-hover">
+                            <table class="table table-borderless">
                                 <thead>
                                     <tr>
                                         <th>Nama Penerima</th>
